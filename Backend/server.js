7cf9edad96619d5a3c3e0e5a68bfc322128bd0c3
Vivek Srivastava <viveksrivastava6180@gmail.com>
2022-10-04 18:07:54 +0530

    Added Socket

--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,13 @@ require("dotenv").config();
 const server = require('http').createServer(app);
 const {login, register} = require('./Routers');
 const mongoose = require("mongoose");
+const io = require('socket.io')(server,{
+  cors:
+  {
+    origin: process.env.CLIENT_URL,
+    allowedHeaders: ["*"],
+  }
+});
 
 app.use(express.urlencoded({ extended : false }));
 app.use(express.json());
@@ -14,8 +21,11 @@ mongoose.connect(process.env.MONGODB_URI,
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
-  .then(() => console.log('DATABASE CONNECTED'))
+  .then(() => {
+    console.log('DATABASE CONNECTED');
+  })
   .catch(err => console.error("Error connecting to mongo", err));
+
 
 app.use((req, res, next) => {
 	res.setHeader('Access-Control-Allow-Origin', process.env.CLIENT_URL);
@@ -23,6 +33,21 @@ app.use((req, res, next) => {
 	res.setHeader('Access-Control-Allow-Credentials', true);
 	next();
 });
+
+io.on('connection', socket => {
+  const id = socket.handshake.query.id
+  socket.join(id)
+
+  socket.on('send-message', ({ recipients, text }) => {
+    recipients.forEach(recipient => {
+      const newRecipients = recipients.filter(r => r !== recipient)
+      newRecipients.push(id)
+      socket.broadcast.to(recipient).emit('receive-message', {
+        recipients: newRecipients, sender: id, text
+      })
+    })
+  })
+})
 
 app.use(login);
 app.use(register);
